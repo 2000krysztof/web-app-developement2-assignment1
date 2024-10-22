2114129d14ad2738774df5049c060ae7278815d8
krzysztof <2000krzysztof@gmail.com>
2024-10-22 12:51:01 +0100

    Exercise: Movie Details Page- included production countries.

--- movies/src/components/movieDetails/index.js
+++ movies/src/components/movieDetails/index.js
@@ -47,6 +47,20 @@ const [drawerOpen, setDrawerOpen] = useState(false);
           </li>
         ))}
       </Paper>
+
+      <Paper 
+        component="ul" 
+        sx={{...root}}
+      >
+        <li>
+          <Chip label="Production Countries" sx={{...chip}} color="primary" />
+        </li>
+        {movie.production_countries.map((g) => (
+          <li key={g.name}>
+            <Chip label={g.name} sx={{...chip}} />
+          </li>
+        ))}
+      </Paper>
       <Paper component="ul" sx={{...root}}>
         <Chip icon={<AccessTimeIcon />} label={`${movie.runtime} min.`} />
         <Chip
